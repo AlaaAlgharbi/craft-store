8d9ad67e42b57a24ae7a0834fb55b0c1cc3f3017
unknown <algarbealaa@gmail.com>
2025-03-27 14:15:40 +0300

    models 2

--- artiauct/settings.py
+++ artiauct/settings.py
@@ -37,18 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'product.apps.ProductConfig' ,
-    'productAuction.apps.ProductauctionConfig'
+    'product',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'artiauct.urls'
--- product/models.py
+++ product/models.py
@@ -4,19 +4,18 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.contenttypes.models import ContentType
 
-
 # Create your models here.
-category = {
+category = [
     ("Accessories", "Accessories"),
     ("Candle", "Candle"),
     ("Painting", "Painting"),
-    ("wool", "wool"),
-    ("sweet", "sweet"),
+    ("Wool", "Wool"),
+    ("Sweet", "Sweet"),
     ("Carving", "Carving"),
     ("Decoration", "Decoration"),
     ("Bags", "Bags"),
     ("other", "other")
-}
+]
 
 
 class Comment(models.Model):
@@ -25,7 +24,9 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
-
+    
+    def __str__(self):
+        return self.content
 
 class Product(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
@@ -37,3 +38,26 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to='photos')
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
     comment = GenericRelation(Comment)
+    
+    def __str__(self):
+        return self.id
+
+class ProductAuction(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    inital_price = models.IntegerField()
+    current_price = models.IntegerField()
+    end_date = models.DateTimeField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=50, choices=category, null=True)
+    image = models.ImageField(null=True, blank=True, upload_to='photos')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user')
+    comment = GenericRelation(Comment)
+    buyer=models.OneToOneField(User, on_delete=models.CASCADE,related_name='buyer')
+    activate=models.BooleanField()
+    
+    def __str__(self):
+        return self.id
+
+
+
