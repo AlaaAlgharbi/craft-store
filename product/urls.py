19c2709fdce4ee85b34cb8cbb9770cd5bc5253e2
unknown <algarbealaa@gmail.com>
2025-03-29 01:38:33 +0300

    product serializer

--- product/urls.py
+++ product/urls.py
@@ -2,10 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("users/create", UserCreate.as_view(), name="user_create"),
     path("users/<str:username>", UserDetail.as_view(), name="user_detail"),
     path("users/", UserList.as_view(), name="user_list"),
-    path("product/creat", ProductCreat.as_view(), name="product_creat"),
     path("products/", ProductList.as_view(), name="product_List"),
-    path("products/<str:name>", ProdutDetails.as_view(), name="product_detail"),
+    path("products/<int:pk>", ProdutDetails.as_view(), name="product_detail"),
 ]
