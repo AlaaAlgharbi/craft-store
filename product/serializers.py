2d37f2b56cbeb506dac01cf8951887e0f1b104bf
unknown <algarbealaa@gmail.com>
2025-03-29 01:38:33 +0300

    product serializer

--- product/serializers.py
+++ product/serializers.py
@@ -1,27 +1,44 @@
+from email.mime import image
 from rest_framework import serializers
-from .models import CustomUser , Product , ProductAuction , CustomUser
+from .models import CustomUser , Product , ProductAuction 
 
-class UserDetailsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ["username","first_name","phone_number","image"]
-
-class SianUpUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    # password = serializers.CharField(write_only=True)
     class Meta:
         model = CustomUser
         fields = ["username","first_name","phone_number","image","password"]
-
-class ListUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ["username","first_name","phone_number","image"]
+        extra_kwargs = {
+            "password": {"write_only": True}}
 
 class ProductSerializer(serializers.ModelSerializer):
+    
+    user_name = serializers.SerializerMethodField()
+    user_image = serializers.SerializerMethodField()
     class Meta :
         model = Product
-        fields = ["name", "price", "created_at", "category", "image", "user" , "description"]
+        fields = ["name","price","category","image","description","user_name","user_image","rate"]
+        read_only_fields  = ["user_image","user_name","rate"]
+        extra_kwargs = {
+            "category": {"write_only": True},
+            "user": {"write_only": True},
+            "description": {"write_only": True},}
+        
+    def get_user_name(self, obj):
+        return obj.user.username if obj.user else None
+    def get_user_image(self, obj):
+        return obj.user.image.url if obj.user and obj.user.image else None
 
+
+        
 class ProductDetailSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    user_image = serializers.SerializerMethodField()
     class Meta : 
         model = Product
-        fields = ["name" , "price" , "rate" , "category"]
+        fields = ["name","price","category","image","description",
+                "user_name","user_image","rate","created_at"]
+        read_only_fields = ["rate","created_at","user_name","user_image"]
+    def get_user_name(self, obj):
+        return obj.user.username if obj.user else None
+    def get_user_image(self, obj):
+        return obj.user.image.url if obj.user and obj.user.image else None 
--- product/views.py
+++ product/views.py
@@ -2,31 +2,21 @@ from rest_framework import generics
 from .serializers import *
 from .models import CustomUser
 
-class UserCreate(generics.CreateAPIView):
+class UserList(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = SianUpUserSerializer
-
-class UserList(generics.ListAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = ListUserSerializer
+    serializer_class = UserSerializer
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     lookup_field="username"
     queryset = CustomUser.objects.all()
-    serializer_class = UserDetailsSerializer
+    serializer_class = UserSerializer
 
 
-class ProductCreat(generics.CreateAPIView):
+class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ProductList(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductDetailSerializer
-
-
 class ProdutDetails(generics.RetrieveUpdateDestroyAPIView):
-    lookup_field="name"
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
