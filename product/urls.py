f35afbce4205212eff6d2fddd30fd32a9c0267cc
unknown <algarbealaa@gmail.com>
2025-04-09 15:01:02 +0300

    fixes

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,6 @@ from .views import *
 urlpatterns = [
     path("users/<str:username>", UserDetail.as_view(), name="user_detail"),
     path("users/", UserList.as_view(), name="user_list"),
-    # path("users/<str:username>", UserList.as_view(), name="user_list"),
     path("products/", AllProductsView.as_view(), name="Product_List"),
     path("create/products/", ProductCreate.as_view(), name="Product_Create"),
     path("create/auction/", ProductAuctionCreate.as_view(), name="ProductAuction_Create"),
@@ -17,9 +16,6 @@ urlpatterns = [
     path("chat/user/<int:user_id>/", UserChatListView.as_view(), name="user-chat-list"),
     path("search/<str:query>/", SearchAllView.as_view(), name="user-search"),
     path("chat/search/<str:message>/", ChatListCreateView.as_view(), name="message-search"),
-    path("notifications/", NotificationListView.as_view(), name="notification-list"),
-    path("notifications/<int:pk>/", NotificationDetailView.as_view(), name="notification-detail"),
-    path("notifications/check/", CheckAuctionNotifications.as_view(), name="check-notifications"),
 ]
 
 
