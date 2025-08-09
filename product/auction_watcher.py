8e04e05da2abbf76a4fead6bc23e308e767a04f7
AlaaAlgharbi <algarbealaa@gmail.com>
2025-08-09 16:21:08 +0300

    stop auction watcher

--- product/auction_watcher.py
+++ product/auction_watcher.py
@@ -1,29 +1,29 @@
-import time
-from django.utils import timezone
+# import time
+# from django.utils import timezone
 
 
-def auction_watcher():
-    from .models import ProductAuction, Notification
-    while True:
-        now = timezone.now()
-        auctions = ProductAuction.objects.filter(
-            end_date__lte=now
-        ).exclude(notifications__notification_type="auction_end")
-        for auction in auctions:
-            # إشعارات للفائز والمالك
-            if auction.buyer:
-                Notification.objects.create(
-                    user=auction.buyer,
-                    auction=auction,
-                    notification_type="auction_won",
-                    message=f"Congratulations! You won the auction for {auction.user} the auction name is {auction.name}",
-                )
+# def auction_watcher():
+#     from .models import ProductAuction, Notification
+#     while True:
+#         now = timezone.now()
+#         auctions = ProductAuction.objects.filter(
+#             end_date__lte=now
+#         ).exclude(notifications__notification_type="auction_end")
+#         for auction in auctions:
+#             # إشعارات للفائز والمالك
+#             if auction.buyer:
+#                 Notification.objects.create(
+#                     user=auction.buyer,
+#                     auction=auction,
+#                     notification_type="auction_won",
+#                     message=f"Congratulations! You won the auction for {auction.user} the auction name is {auction.name}",
+#                 )
 
-            Notification.objects.create(
-                user=auction.user,
-                auction=auction,
-                notification_type="auction_end",
-                message=f"your auction {auction.name} is end , and {auction.buyer} win the auction"
-            )
+#             Notification.objects.create(
+#                 user=auction.user,
+#                 auction=auction,
+#                 notification_type="auction_end",
+#                 message=f"your auction {auction.name} is end , and {auction.buyer} win the auction"
+#             )
 
-        time.sleep(30)  
+#         time.sleep(30)  
